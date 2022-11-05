5f6d4c8f572bb94121fc7f985ece1beea6889b0e
Amine Ounissi <73407173+Amine-Ounissi@users.noreply.github.com>
2022-11-05 23:47:38 +0100

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,11 +1,6 @@
-def buildJar() {
+def buildAnsible() {
   echo "building the application..."
   sh'npm install --legacy-peer-deps'
-  sh'ng build'
-}
-
-def buildAnsible() {
-  echo "building ansible..."
   sh'ansible-playbook ansible/build.yml -i ansible/inventory/host.yml'
 }
 
